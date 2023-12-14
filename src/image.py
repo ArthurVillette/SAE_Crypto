2a251fbac5856a2e39efaca38b390b2e445d04ba
RomainMechain <romain.mechain@etu.univ-orleans.fr>
2023-12-14 12:13:02 +0100

    Feat/ Ajout de la fonction get_message_image

--- src/image.py
+++ src/image.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 def analyse_image(chemin:str)->None:
-    """la fonction affiche l'image
+    """la fonction retourne l'image en binaire en fonction de si ces pixels sont paire ou non
 
     Args:
         chemin (_String_): le chemin de l'image
@@ -17,3 +17,15 @@ def analyse_image(chemin:str)->None:
             else:
                 res += "1"
     return res
+
+def get_message_image(chemin:str)->str :
+    """ Renvoit le message caché dans l'image
+
+    Args:
+        chemin (str): Le chemin de l'image 
+
+    Returns:
+        str: le message caché
+    """    
+    res = analyse_image(chemin)
+    return res[:64]
