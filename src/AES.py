b14e2c03c788b62be790f8bbc078fa40c1265665
RomainMechain <romain.mechain@etu.univ-orleans.fr>
2023-12-17 11:31:56 +0100

    Doc/ Ajout des docstring et des annotator manquants

--- src/AES.py
+++ src/AES.py
@@ -5,7 +5,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
 import os
 
-def cryptage_AES (message, clee_chiffrage):
+def cryptage_AES (message:str, clee_chiffrage:str):
     """la fonction renvoie le message crypté avec la méthode AES
 
     Args:
@@ -13,7 +13,7 @@ def cryptage_AES (message, clee_chiffrage):
         cle (_String_): la clé de cryptage
 
     Returns:
-        _String_: le message crypté
+        bytes: le message crypté
     """
     clee_chiffrage = clee_chiffrage.ljust(32)[:32].encode()
     iv = os.urandom(16)
@@ -26,11 +26,11 @@ def cryptage_AES (message, clee_chiffrage):
     return iv + texteCrypte
 
 
-def decrypt_AES(texteCrypte, clee_chiffrage):
+def decrypt_AES(texteCrypte:bytes, clee_chiffrage:str):
     """la fonction renvoie le message décrypté avec la méthode AES
 
     Args:
-        texteCrypte (_String_): le message à décrypter
+        texteCrypte (bytes): le message à décrypter
         cle (_String_): la clé de décryptage
         
     Returns:
@@ -47,17 +47,17 @@ def decrypt_AES(texteCrypte, clee_chiffrage):
     return message.decode()
 
 
-def cryptage_AES_CBC (message, clee_chiffrage, iv):
-    """la fonction renvoie le message crypté avec la méthode AES
+def cryptage_AES_CBC (message:str, clee_chiffrage:bytes, iv:bytes):
+    """la fonction renvoie le message crypté avec la méthode AES, en mode CBC
 
     Args:
         message (_String_): le message à crypter
-        cle (_String_): la clé de cryptage
+        cle (bytes): la clé de cryptage
+        iv (bytes) : le vecteur d'initialisation
 
     Returns:
-        _String_: le message crypté
+        bytes: le message crypté
     """
-    #clee_chiffrage = clee_chiffrage.ljust(32)[:32].encode()
     cipher = Cipher(algorithms.AES(clee_chiffrage), modes.CBC(iv), backend=default_backend())
     padder = padding.PKCS7(128).padder()
     data = padder.update(message.encode()) + padder.finalize()
@@ -66,31 +66,20 @@ def cryptage_AES_CBC (message, clee_chiffrage, iv):
 
     return texteCrypte
 
-def decrypt_AES_CBC(texteCrypte, clee_chiffrage, iv):
-    """la fonction renvoie le message décrypté avec la méthode AES
+def decrypt_AES_CBC(texteCrypte:bytes, clee_chiffrage:bytes, iv:bytes):
+    """la fonction renvoie le message décrypté avec la méthode AES, en mode CBC
 
     Args:
-        texteCrypte (_String_): le message à décrypter
-        cle (_String_): la clé de décryptage
+        texteCrypte (bytes): le message à décrypter
+        cle (bytes): la clé de décryptage
+        iv (bytes) : le vecteur d'initialisation 
         
     Returns:
         _String_: le message décrypté
     """
-    #clee_chiffrage = clee_chiffrage.ljust(32)[:32].encode()
     cipher = Cipher(algorithms.AES(clee_chiffrage), modes.CBC(iv), backend=default_backend())
     decryptor = cipher.decryptor()
     data = decryptor.update(texteCrypte) + decryptor.finalize()
     unpadder = padding.PKCS7(128).unpadder()
     message = unpadder.update(data) + unpadder.finalize()
     return message.decode()
-
-
-
-# cle_256 = 0b1110011101101101001100010011111110010010101110011001000001001100111001110110110100110001001111111001001010111001100100000100110011100111011011010011000100111111100100101011100110010000010011001110011101101101001100010011111110010010101110011001000001001100
-# cle_256_bytes = cle_256.to_bytes(32, "big")
-
-# iv = iv = os.urandom(16)
-# print(iv)
-# temp = cryptage_AES_CBC("un dernier message", cle_256_bytes, iv)
-# print(temp)
-# print(decrypt_AES_CBC(temp, cle_256_bytes, iv))
